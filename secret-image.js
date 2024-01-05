7bfcee93d47d3a7626007fc83fb6a7210b125880
Justine Bayron <Jbayron76@gmail.com>
2024-01-05 21:40:20 +0800

    export and import 2

--- secret-image.js
+++ secret-image.js
@@ -1,10 +1,11 @@
-/* secret-messages.js */
+/* secret-image.js */
 import { toggleHiddenElement, changeToFunkyColor } from "./dom-function";
 
 const buttonElement = document.getElementById('secret-button');
-const pElement = document.getElementById('secret-p');
+const imgElement = document.getElementById('secret-img');
+
 
 buttonElement.addEventListener('click', () => {
-  toggleHiddenElement(pElement);
-  changeToFunkyColor(pElement);
+  toggleHiddenElement(imgElement);
+  changeToFunkyColor(imgElement);
 });
