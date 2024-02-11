51e94c5975e8b7227611dae7ceab295830e59f7c
Justine Bayron <Jbayron76@gmail.com>
2024-02-11 19:46:46 +0800

    just a few changes

--- React/myfirstreact/src/Components/TodoForm.js
+++ React/myfirstreact/src/Components/TodoForm.js
@@ -4,15 +4,17 @@ export const TodoForm = ({addTodo}) => {
   const [value, setValue] = useState("");
   const hadleSubmit = (e) => {
     e.preventDefault();
-    addTodo(value);
-    setValue("");
+    if (value !== '') {
+      addTodo(value);
+      setValue("");
+    }
   }
 
   return (
     <form className='TodoForm' onSubmit={hadleSubmit}>
       <input className='todo-input' placeholder='What is the task today?' value={value} onChange={(e) => {
         setValue(e.target.value);
-      }}/>
+      }} required/>
       <button type='submit' className='todo-btn'>Add Task</button>
     </form>
   )
