35a9e031d455c4ca952ca7f731b2fd7d0e956ae3
Lord Byron <Jbayron76@gmail.com>
2025-04-10 22:48:18 +0800

    learning routingx

--- Laravel/quickstart/app/Http/Controllers/routes.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-Route::get('/greeting', function () {
-    return 'Hello World';
-});
-
-?>
--- Laravel/quickstart/routes/web.php
+++ Laravel/quickstart/routes/web.php
@@ -2,11 +2,21 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/greeting', function () {
-    return 'Hello World';
-});
+Route::view('/', 'welcome');
 
+Route::get('/user/{username?}', function (?string $username = 'Justine') {
+    return 'Hello World ' . $username;
+})->where('username', '[A-Z]+');
+
+
+Route::get('/whatever', function () {
+    return view('whatever');
+})->name('whatever'); 
+
+$url = route('whatever');
+
+Route::redirect('/x', '/whatever'); 
