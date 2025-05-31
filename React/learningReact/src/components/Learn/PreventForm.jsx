46c5356ae05afdabf7f554a80c24586d22f07d54
Lord Byron <Jbayron76@gmail.com>
2025-05-31 15:05:18 +0800

    🔹 9. Keyboard Events

--- React/learningReact/src/components/Learn/PreventForm.jsx
+++ React/learningReact/src/components/Learn/PreventForm.jsx
@@ -5,19 +5,29 @@ const PreventForm = () => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
+
+        if (name.trim() === '') return;
+
         alert('Form submitted without reload!');
         alert(`Name: ${name}`);
         console.log('Form submitted without reload!');
+        setName('');
     }
 
     const handleNameChange = (e) => {
         setName(e.target.value);
     }
 
+    const handleKeyDown = (e) => {
+        if (e.key === 'Enter') {
+            alert('ENTER IS PRESSED');
+        }
+    }
+
     return(
         <form onSubmit={handleSubmit}>
             <label htmlFor="name">Name:</label>
-            <input type="text" name="name" id="namee" value={name} onChange={handleNameChange}/>
+            <input type="text" name="name" id="namee" value={name} onChange={handleNameChange} onKeyDown={handleKeyDown}/>
             <button type="submit">Submit</button>
         </form>
     );
