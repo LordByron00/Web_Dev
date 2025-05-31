68d178eab1d2ccc16e4c4672ac1ee4ee005f761a
Lord Byron <Jbayron76@gmail.com>
2025-05-31 14:56:20 +0800

    Handling Input (onChange)

--- React/learningReact/src/components/Learn/PreventForm.jsx
+++ React/learningReact/src/components/Learn/PreventForm.jsx
@@ -1,12 +1,22 @@
+import { useState } from "react";
+
 const PreventForm = () => {
+    const [name, setName] = useState('');
+
     const handleSubmit = (e) => {
         e.preventDefault();
         alert('Form submitted without reload!');
         console.log('Form submitted without reload!');
     }
 
+    const handleNameChange = (e) => {
+        setName(e.target.value);
+    }
+
     return(
         <form onSubmit={handleSubmit}>
+            <label htmlFor="name">Name:</label>
+            <input type="text" name="name" id="namee" value={name} onChange={handleNameChange}/>
             <button type="submit">Submit</button>
         </form>
     );
