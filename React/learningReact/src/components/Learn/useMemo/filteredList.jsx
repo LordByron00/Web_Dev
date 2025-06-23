4ad7db5e5bde89f2bb09a8014d4759e9d3158bf7
Lord Byron <Jbayron76@gmail.com>
2025-06-23 23:00:20 +0800

    usememo

--- React/learningReact/src/components/Learn/useMemo/filteredList.jsx
+++ React/learningReact/src/components/Learn/useMemo/filteredList.jsx
@@ -1,13 +1,18 @@
-const filteredList = () => {
+import { useMemo, useState } from "react";
 
+const filteredList = ({ items }) => {
+    const [query, setQuery] = useState("");
+
+    const filteredItems = useMemo(() => {
+                return items.filter(item => item.toLowerCase().includes(query.toLowerCase()));
+    }, [items, query]);
+    
     return(
         <>
-            <div>xxx</div>
-            <div>xxx</div>
-            <div>xxx</div>
-  <div>xxx</div>
-            <div>xxx</div>
-            <div>xxx</div>
+            <input type="text" name="" id="" value={query} onChange={e => setQuery(e.target.value)}/>
+            <ul>
+                {filteredItems.map(i => <li key={i}>{i}</li>)}
+            </ul>
         </>
     );
 }
