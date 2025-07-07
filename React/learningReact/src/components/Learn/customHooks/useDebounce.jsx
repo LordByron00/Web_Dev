2b7bf3f27197e43be7125b7ee62dec3e3e365af2
Lord Byron <Jbayron76@gmail.com>
2025-07-07 22:28:19 +0800

    Custom hooks useDebounce

---
+++ React/learningReact/src/components/Learn/customHooks/useDebounce.jsx
@@ -0,0 +1,12 @@
+import { useEffect, useState } from "react";
+
+function useDebounce(value, delay = 500) {
+    const [debounce, setDebounce] = useState(value, delay);
+
+    useEffect(() => {
+        const timer = setTimeout(() => setDebounce(value), delay);
+        clearInterval(timer);
+    }, [value, delay]);
+
+    return debounce;
+}
