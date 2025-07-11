ab8f2fa0b22996606e9a845e8d8f143c342ea9d7
Lord Byron <Jbayron76@gmail.com>
2025-07-11 23:26:31 +0800

    Timer App core

--- React/learningReact/src/components/Learn/Timer/Timer.jsx
+++ React/learningReact/src/components/Learn/Timer/Timer.jsx
@@ -1,13 +1,19 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Timer = (initialTime = 0) => {
     const [time, setTime] = useState(initialTime)
     const [isActive, setActive] = useState(false);
     const [isPaused, setPause] = useState(false);
     
-    function setTimer() {
-        let interval = setInterval(() => setTime(prev => prev + 1), 1000);
-    }
+    useEffect(() => {
+        let interval = null
+        if (isActive && !isPaused) {
+            interval = setInterval(() => setTime(prev => prev + 1), 1000);
+        } else {
+            clearInterval(interval);
+        }
+
+    }, [isActive, isPaused]);
 
     const start = () => {
         setActive(true);
@@ -29,7 +35,7 @@ const Timer = (initialTime = 0) => {
         setActive(false);
     }
 
-    return {time, setTimer, start, pause, resume, reset}
+    return {time, isActive, isPaused, start, pause, resume, reset}
 }
 
 export default Timer;
